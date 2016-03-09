07a4480d7458e538d989e760e6b62eb2df2dc613
Marisma <Marisma@WIN-L93IRKH2F9P>
2016-03-09 11:46:08 +0100

    Después añadimos las líneas siguientes: volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3); System.out.println("El volumen de la esfera cuyo radio "+radio+" es "+volumen); System.out.println("Finalizado Version 1");

--- CalculoVolumenes/src/Prog1_0.java
+++ CalculoVolumenes/src/Prog1_0.java
@@ -9,5 +9,8 @@ public class Prog1_0 {
 		System.out.println("Introduce el radio de la esfera");
 		radio = leerradio.nextInt();
 		double volumen;
+		volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
+		System.out.println("El volumen de la esfera cuyo radio "+radio+" es	"+volumen);
+		System.out.println("Finalizado Version 1");
 	}
 }
